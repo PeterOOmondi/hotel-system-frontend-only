6d09c1ddfbe0fb6227278c3518fd4f8f70018227
Peter Ochieng <106829161+PeterOOmondi@users.noreply.github.com>
2024-09-11 22:54:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello!</h1>
+      <h1>
+          This is all about the hotel management system, the frontend side.
+    </h1>
     </div>
   );
 }
